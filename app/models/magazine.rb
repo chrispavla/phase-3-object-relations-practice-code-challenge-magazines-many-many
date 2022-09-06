56482adc71ef36d37e995b50da0937792963a6e4
Kristina <kristina.hodges7@gmail.com>
2022-09-05 20:08:44 -0400

    commit tree a848af1c0377311a1951185ee45424128b85e1aa

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,12 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+  def email_list
+  end
+
+  def self.most_popular
+    Magazine.maximum()
+  end
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,15 @@
 class Reader < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
+
+  def subscribe(magazine, price)
+
+  end
+
+  def total_subcription_price
+  end
+
+  def cancel_subscription(magazine)
+  end
+
 end
